c31c399e81720de61d3e544a340bc5664a674712
Mohd Rejoan <wearecrx@gmail.com>
2021-09-13 23:33:56 +0600

    home and scroll animation complete

---
+++ js/script.js
@@ -0,0 +1,194 @@
+// slider 
+
+document.addEventListener("DOMContentLoaded", function () {
+    new Splide(".splide", {
+        type: "loop",
+        perPage: 3,
+        focus: "center",
+        autoWidth: false,
+    }).mount();
+});
+
+// aos animation 
+AOS.init({
+    duration: 600
+})
+
+// smooth scroll bar 
+
+// var Scrollbar = window.Scrollbar;
+
+// Scrollbar.init(document.querySelector("#full__container"), {
+//     dumping: 0.05
+// });
+
+
+// gsap animation 
+// bottom - texts;
+
+// get all texts
+// bottom texts  
+const leftText = gsap.utils.toArray("#bottom-texts #serious path");
+const rightText = gsap.utils.toArray("#bottom-texts #seeds path");
+const bottomTextGradient = gsap.utils.toArray("#bottom-texts #circle-gradient");
+// top logo 
+const topMainCircle = gsap.utils.toArray("#top-logo #main-circle");
+const topSeedContainer = gsap.utils.toArray("#top-logo #seedContainer");
+const topContainerSeed = gsap.utils.toArray("#top-logo #seed");
+// const topCenterCircleGradient = gsap.utils.toArray("#top-logo #inner-dot-group");
+
+gsap.set(bottomTextGradient, {
+    display: 'none'
+})
+gsap.set(
+    gsap.utils.toArray(topMainCircle, topSeedContainer),
+    {
+        transformOrigin: "center center",
+    }
+);
+
+
+
+gsap.timeline()
+    .fromTo(
+        [leftText, rightText],
+        {
+            y: 100,
+            opacity: 0,
+        },
+        {
+            y: 0,
+            opacity: 1,
+            delay: 1,
+            stagger: {
+                amount: 2,
+            },
+        }
+    )
+    .fromTo(
+        topMainCircle,
+        {
+            scale: 0,
+        },
+        {
+            scale: 1,
+            duration: 2,
+        },
+        "<"
+    )
+    .fromTo(
+        topSeedContainer,
+        {
+            scale: 0,
+        },
+        {
+            scale: 1,
+            duration: 1.2,
+        },
+        "-=1"
+    )
+
+    .fromTo(
+        topContainerSeed[0],
+        {
+            x: 500,
+            y: 250,
+            fill: "#FFFFFF",
+            stroke: "#FFFFFF",
+        },
+        {
+            x: 0,
+            y: 0,
+            fill: "#000000",
+            stroke: "#000000",
+            duration: 1.8,
+        }
+    )
+    .fromTo(
+        topContainerSeed[1],
+        {
+            x: -500,
+            fill: "#FFFFFF",
+            stroke: "#FFFFFF",
+        },
+        {
+            x: 0,
+            fill: "#000000",
+            stroke: "#000000",
+            duration: 1.8,
+        },
+        "<"
+    )
+    .fromTo(
+        topContainerSeed[2],
+        {
+            x: -500,
+            y: 500,
+            fill: "#FFFFFF",
+            stroke: "#FFFFFF",
+        },
+        {
+            x: 0,
+            y: 0,
+            fill: "#000000",
+            stroke: "#000000",
+            duration: 1.8,
+        },
+        "<"
+    )
+    .to("#animation__wrap", {
+        height: "0",
+        duration: 2,
+    })
+    .set("#animation__wrap", {
+        display: 'none'
+    }).fromTo(
+            "#top__text",
+            {
+                opacity: 0,
+                y: 50,
+            },
+            {
+                opacity: 1,
+                y: 0,
+                duration: 0.8,
+            }
+        )
+        .fromTo(
+            ".brand #brand__svg",
+            {
+                opacity: 0,
+                x: 50,
+                y: 50,
+            },
+            {
+                opacity: 1,
+                x: 0,
+                y: 0,
+                duration: 1,
+            },
+            "<"
+        )
+        .fromTo(
+            "#highlightText h1",
+            {
+                y: 100,
+                opacity: 0,
+            },
+            {
+                y: 0,
+                opacity: 1,
+                stagger: {
+                    each: 0.5,
+                },
+                duration: 1
+            }
+        )
+        .fromTo(".bottom__texts", {
+            y: 50,
+            opacity: 0,
+        }, {
+            y: 0,
+            opacity: 1,
+            duration: 0.8
+        });
